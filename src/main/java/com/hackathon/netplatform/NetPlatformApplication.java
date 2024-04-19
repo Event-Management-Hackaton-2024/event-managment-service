b28dfcd109cc7fc5e028345ebeb72f25cb32a7ea
ignatIgnatov <ignatov_rs@abv.bg>
2024-04-19 22:48:07 +0300

    initial commit

---
+++ src/main/java/com/hackathon/netplatform/NetPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.netplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/netplatform/NetPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.netplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
